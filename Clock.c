82a1af476ee1cda936b8f055bf28fe4f1ae3c1aa
Komidass <thomas1871997@gmail.com>
2020-04-14 22:04:54 +0200

    fixed the setting alarm bug where the inputs were not taken correctly, the am/pm alarm setting is still not yet supported

--- Clock.c
+++ Clock.c
@@ -21,20 +21,21 @@ u8 Seconds = 0;
 u8 Minutes = 0;
 u8 Hours = 0;
 u8 AM_PM = AM;
-u8 Alarm_Minutes;
-u8 Alarm_Hours;
-u8 Alarm_AM_PM;
-u8 flags;
+u8 Alarm_Minutes = 0;
+u8 Alarm_Hours = 0;
+u8 Alarm_AM_PM = 0;
+u8 flags = 0;
 u8 KPD_Check_frequency = KPD_Check_frequency_Slow;
 SemaphoreHandle_t LCD ;
 xQueueHandle KPD_input = NULL;
 xQueueHandle minutes_adjusted = NULL; //for adjusted minutes to add in the hours
 TaskHandle_t Minutes_handle = NULL;
 TaskHandle_t Hours_handle = NULL;
-
+TaskHandle_t Alarm_handle = NULL;
 
 void Clock_Print_Default_Interface(void)
 {
+	LCD_Void_Clear();
 	LCD_Set_Block(hours_position);
 	LCD_Void_Write_Number_2((Hours%12)+1);
 	LCD_Set_Block(minutes_position-1);
@@ -43,6 +44,9 @@ void Clock_Print_Default_Interface(void)
 	LCD_Set_Block(Am_PM_position);
 	if(AM_PM == AM) LCD_Void_Write_String("AM");
 	else LCD_Void_Write_String("PM");
+	LCD_Set_Block(Alarm_Icon_Position);
+	if(Get_Bit(flags,alarm_set) == 1) LCD_Void_Write_Data(Pixel_Alarm);
+	else LCD_Void_Write_Data(' ');
 	LCD_Set_Block(seconds_position);
 	LCD_Void_Write_Number_2(Seconds);
 
@@ -61,6 +65,7 @@ void Clock_Second(void *pvParameters)
 			LCD_Void_Write_Number_2(Seconds);
 			xSemaphoreGive(LCD);
 		}
+
 		vTaskDelayUntil(&MyLastUnblockS,seconds_frequency);
 		Seconds++;
 		Seconds %= 60;
@@ -77,20 +82,19 @@ void Clock_Minute(void *pvParameters)
 	{
 		if(Get_Bit(flags,alarm_set) == 1)//alarm is set
 		{
-			LCD_Set_Block(26);
-			LCD_Void_Write_Number_2(Alarm_Hours);
-			LCD_Void_Write_String("?=");
-			LCD_Void_Write_Number_2(Alarm_Minutes);
-
 			if((Hours == Alarm_Hours)&&(Minutes == Alarm_Minutes))
 			{
-				Clock_Alarm();
+				xTaskCreate(Clock_Alarm,"Alarm",50,NULL,4,&Alarm_handle);
 			}
 		}
 		if(xSemaphoreTake(LCD,10))
 		{
+
 			LCD_Set_Block(minutes_position);
 			LCD_Void_Write_Number_2(Minutes);
+			LCD_Set_Block(Alarm_Icon_Position);
+			if(Get_Bit(flags,alarm_set) == 1) LCD_Void_Write_Data(Pixel_Alarm);
+			else LCD_Void_Write_Data(' ');
 			xSemaphoreGive(LCD);
 		}
 
@@ -201,25 +205,53 @@ void Clock_Typing_Up_Arrow(u8* current_block)// what happens when up arrrow is p
 	}
 }
 
+void Clock_Typing_Alarm_Button(void)
+{
+	//entering alarm setting mode
+	if(Get_Bit(flags,alarm_adjust) == 0)
+	{
+		LCD_Set_Block(second_row_start);
+		LCD_Void_Write_Data(Pixel_Arrow);
+		Set_Bit(flags,alarm_adjust);
+		LCD_Set_Block(hours_position);
+		LCD_Void_Write_Number_2(0);
+		LCD_Set_Block(minutes_position-1);
+		LCD_Void_Write_Data(':');
+		LCD_Void_Write_Number_2(0);
+		LCD_Set_Block(Am_PM_position);
+		LCD_Void_Write_String("AM");
+		LCD_Set_Block(Set_Alarm_Position);
+		LCD_Void_Write_String("Set Alarm");
+	}
+	//exiting alarm setting mode
+	else
+	{
+		LCD_Void_Clear();
+		Clear_Bit(flags,alarm_adjust);
+		Clock_Print_Default_Interface();
+
+	}
+}
+
 void Clock_Typing_Number(u8* pressed,u8* time_adjusted,u8* current_block,u8* hours,u8* minutes)//what happens when a number is pressed in typing mode
 {
 	/*change the time according to the user input*/
 	switch (*current_block)
 			{
 			case hours_position+16:
-				*hours = ((*pressed-'0')*10)+(Hours%10);
+				*hours = ((*pressed-'0')*10)+(*hours%10);
 				*time_adjusted = 1;
 				break;
 			case hours_position+17:
-				*hours = (*pressed-1-'0')+((Hours/10)*10);
+				*hours = (*pressed-1-'0')+((*hours/10)*10);
 				*time_adjusted = 1;
 				break;
 			case minutes_position+16:
-				*minutes = ((*pressed-'0')*10)+(Minutes%10);
+				*minutes = ((*pressed-'0')*10)+(*minutes%10);
 				*time_adjusted = 1;
 				break;
 			case minutes_position+17:
-				*minutes = (*pressed-'0')+((Minutes/10)*10);
+				*minutes = (*pressed-'0')+((*minutes/10)*10);
 				*time_adjusted = 1;
 				break;
 			}
@@ -290,11 +322,40 @@ void Clock_Typing_Enter(u8* take_lcd,u8* take_lach,u8* current_block)//what happ
 		KPD_Check_frequency = KPD_Check_frequency_Fast; // enter fast KPD checking freq
 	}
 }
+
+void Clock_Print_Alarm_Interface(void)
+{
+	LCD_Void_Clear();
+	LCD_Set_Block(hours_position);
+	LCD_Void_Write_Number_2((Alarm_Hours%12)+1);
+	LCD_Set_Block(minutes_position-1);
+	LCD_Void_Write_Data(':');
+	LCD_Void_Write_Number_2(Alarm_Minutes%60);
+	LCD_Set_Block(Am_PM_position);
+	if(AM_PM == AM) LCD_Void_Write_String("AM");
+	else LCD_Void_Write_String("PM");
+	LCD_Set_Block(Alarm_Text_Position);
+	if(Get_Bit(flags,alarm_text_toggle) == 1) LCD_Void_Write_String("ALARM!");
+	else LCD_Void_Write_String("      ");
+}
+
 void Clock_Alarm(void)//what happens when there is an alarm
 {
-	LCD_Set_Block(24);
-	LCD_Void_Write_String("ALARM");
-	Clear_Bit(flags,alarm_set);
+	Toggle_Bit(flags,alarm_text_toggle);
+	if(Get_Bit(flags,alarm_set) == 1)//if an alarm is set
+	{
+		if(xSemaphoreTake(LCD,10))
+		{
+			Clock_Print_Alarm_Interface();
+		}
+	}
+	else
+	{
+		Clock_Print_Default_Interface();
+		xSemaphoreGive(LCD);
+		vTaskDelete(Alarm_handle);
+	}
+	vTaskDelay(seconds_frequency/2);
 
 }
 void Clock_Typing_Mode(void *pvParameters)
@@ -332,36 +393,19 @@ void Clock_Typing_Mode(void *pvParameters)
 						break;
 
 					case 'A'://alarm adjusting
-						if(Get_Bit(flags,alarm_adjust) == 0)
-						{
-							Set_Bit(flags,alarm_adjust);
-							LCD_Set_Block(hours_position);
-							LCD_Void_Write_Number_2(0);
-							LCD_Set_Block(minutes_position-1);
-							LCD_Void_Write_Data(':');
-							LCD_Void_Write_Number_2(0);
-							LCD_Set_Block(Am_PM_position);
-							LCD_Void_Write_String("AM");
-							//LCD_Set_Block(23);
-							//LCD_Void_Write_String("Set Alarm");
-						}
-						else
-						{
-							LCD_Void_Clear();
-							Clear_Bit(flags,alarm_adjust);
-							Clock_Print_Default_Interface();
-
-						}
+						Clock_Typing_Alarm_Button();
 						break;
 					default:
-
+						//in time adjusting mode
 						if(Get_Bit(flags,alarm_adjust) == 0)
 						{
 							Clock_Typing_Number(&pressed,&time_adjusted,&current_block,&Hours,&Minutes);
 						}
+						//in alarm setting mode
 						else
 						{
 							Clock_Typing_Number(&pressed,&time_adjusted,&current_block,&Alarm_Hours,&Alarm_Minutes);
+							//alarm is set
 							if(time_adjusted == 1)
 							{
 								Set_Bit(flags,alarm_set);
@@ -390,6 +434,12 @@ void Clock_Typing_Mode(void *pvParameters)
 				else
 				{
 					Clock_Typing_Exit(&take_lcd,&take_lach,&time_adjusted,&current_block);
+				}
+				break;
+			case 'v'://pressing the alarm button any time or at the alarm firing will cancel the alarm or close it
+				if(Get_Bit(flags,alarm_set) == 1)
+				{
+					Clear_Bit(flags,alarm_set);
 				}
 				break;
 			}
--- Clock.h
+++ Clock.h
@@ -16,6 +16,9 @@
 #define minutes_position 3
 #define hours_position 	 0
 #define Am_PM_position 6
+#define Set_Alarm_Position 23
+#define Alarm_Icon_Position 9
+#define Alarm_Text_Position 21
 
 #define second_row_start 16
 #define KPD_Check_frequency_Slow configTICK_RATE_HZ*2
@@ -29,19 +32,35 @@
 
 #define alarm_set 0 // alarm is set
 #define alarm_adjust 1 // enter alarm adjusting mode
+#define alarm_text_toggle 2//to toggle the alarm string text
 
-
+//what happens every second
 void Clock_Second(void *pvParameters);
+//what happens every minute
 void Clock_Minute(void *pvParameters);
+//what happens every hour
 void Clock_Hours(void *pvParameters);
 void Clock_Check_KPD(void *pvParameters);
+//what happens in the typing mode
 void Clock_Typing_Mode(void *pvParameters);
+//the default interface of the clock
 void Clock_Print_Default_Interface(void);
+//what happens when the right arrow is pressed in typing mode
 void Clock_Typing_Right_Arrow(u8* current_block);
+//what happens when the left arrow is pressed in typing mode
 void Clock_Typing_Left_Arrow(u8* current_block);
+//what happens when the up arrow is pressed in typing mode
 void Clock_Typing_Up_Arrow(u8* current_block);
+//what happens when an number is pressed in typing mode
 void Clock_Typing_Number(u8* pressed,u8* time_adjusted,u8* current_block,u8* hours,u8* minutes);
+//what happens when you press the alarm button
+void Clock_Typing_Alarm_Button(void);
+//what happens when exiting typing mode
 void Clock_Typing_Exit(u8* take_lcd,u8* take_lach,u8* time_adjusted,u8* current_block);
+//what happens when you enter typing mode
 void Clock_Typing_Enter(u8* take_lcd,u8* take_lach,u8* current_block);
+//what happens in the alarm
 void Clock_Alarm(void);
+//print the alarm interface
+void Clock_Print_Alarm_Interface(void);
 #endif /* CLOCK_H_ */
